a3bb220cdf2b1737733103ac8f53162f51497c48
khalil1fs <aboulouanek@gmail.com>
2023-03-30 10:04:29 +0000

    refactor

--- snmp/job_checker.pl
+++ snmp/job_checker.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 #=============================================================#
 # Auteur : khalil1fs                                          #
-# But    : Plugin Nagios CPU Usage                            #  
+# But    : Job checker Plugin                                 #  
 # Date   : 28/03/2023                                         #  
 #=============================================================#
  
@@ -16,11 +16,10 @@ use LWP::Simple;
 my $Name = "Data Checker";
 my $Version = "1.0.0";
 my $copyright = "Copyright © Khalil1fs 2023 - tous droits réservés.";
-my $request_url = "https://webhook.site/fef8ca9d-a0f7-46b4-b489-59efed2caea7";
 my $o_help=	undef; 		    # wan't some help ?
-my $o_url = undef; 		    # Request Url
-my $o_crit = undef; 		# la valeur critique
-my $o_warn = undef; 		# la valeur warnning
+my $o_url = undef; 		    # Request Url for jobs
+my $o_crit = undef; 		    # critical for critical status 
+my $o_warn = undef; 		    # critical for warnning status 
 
 
 check_options();
@@ -37,22 +36,28 @@ my %jobs_by_status;
 prepare();
 
 # print the jobs grouped by status
-print_result('Ok', 0, @{$jobs_by_status{'Ok'}});
-print_result('Pending', 1, @{$jobs_by_status{'Pending'}});
-print_result('Fail', 2, @{$jobs_by_status{'Fail'}});
+foreach my $s ('Ok', 'Pending', 'Fail') {
+   print_result($s, 0, @{$jobs_by_status{$s}});
+}
 
 
+sub check_ok_status {
+   my ($job) = @_;
+   my $status_by_timing = get_status_by_timing($job->{'time'});
+   if ($status_by_timing == 0) {
+      push @{$jobs_by_status{'Ok'}}, $job;
+   } elsif ($status_by_timing == 1) {
+      push @{$jobs_by_status{'Pending'}}, $job;    
+   } else {
+      push @{$jobs_by_status{'Fail'}}, $job;
+   }
+}
+
+# prepare the list of jobs and group by ('Ok', 'Pending', 'Fail') status 
 sub prepare {
      foreach my $job (@{$result_array}) {
          if ($job->{'status'} eq 'Ok') {
-            my $status_by_timing = get_status_by_timing($job->{'time'});
-         if ($status_by_timing == 0) {
-           push @{$jobs_by_status{'Ok'}}, $job;
-         } elsif ($status_by_timing == 1) {
-           push @{$jobs_by_status{'Pending'}}, $job;    
-         } else {
-           push @{$jobs_by_status{'Fail'}}, $job;
-         }
+           check_ok_status($job);    
       }
       elsif ($job->{'status'} eq 'Pending') {
          if (get_status_by_timing($job->{'time'}) <= 1) {
@@ -67,6 +72,7 @@ sub prepare {
     }
 }
 
+# print the final result
 sub print_result {
     my ($status, $code, @jobs) = @_;
     my $jobs_length = scalar(@jobs);
@@ -93,46 +99,45 @@ sub get_status_by_timing {
    }    
 }
 
+
 sub check_options {
     Getopt::Long::Configure ("bundling");
     GetOptions(
-        'h'     => \$o_help,    	'help'        	=> \$o_help,
+        'h'     => \$o_help,    	   'help'        	=> \$o_help,
         'u:s'   => \$o_url,   		'url:s'	    => \$o_url,
         'c:s'   => \$o_crit,        'critical:s'    => \$o_crit,
         'w:s'   => \$o_warn,        'warn:s'        => \$o_warn,
 	);
 	
     if (defined ($o_help)) { help(); exit(0)};
-    # check snmp information
+
     if (!defined($o_url) || !defined($o_warn) || !defined($o_crit))
         { print "Put all required params!\n"; print_usage(); exit(3)}
-	if ($o_crit < $o_warn){
-		print "ATTENTION : critique < warning";
-		exit(3);
-	}
+	 
+    if ($o_crit < $o_warn)
+        { print "ATTENTION : critique < warning"; 	exit(3);}
 }
 
 
 sub print_usage {
-    print "Usage: $Name -u <request url> -w <warn level> -c <crit level>\n";
+    print "Usage: $Name -u <request url for jobs> -w <warn level> -c <crit level>\n";
 }
 
 sub help {
    print "\n$Name";
-   print "\nData Checker\n";
    print "MIT licence, (c)2023 Khalil1fs\n\n";
    print_usage();
    print <<EOT;
 -h, --help
    print this help message 
 -w, --warn=INTEGER | INT,INT,INT
-   warning level for cpu in percent 
+   warning level for job timing
 -c, --crit=INTEGER | INT,INT,INT
-   critical level for cpu in percent 
+   critical level for job timing 
 -u, --url=STRING
-   api url to get response
+   request url for jobs
 -V, --Version
-	Print version
+	Print release version
 EOT
 }
 
---
+++ snmp/test.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl -w
+#=============================================================#
+# Auteur : khalil1fs                                          #
+# But    : Example                                            #  
+# Date   : 28/03/2023                                         #  
+#=============================================================#
+ 
+use warnings;
+use strict;
+
+my %all_status = ('Ok', 'Pending', 'Fail');
+
+ foreach my $stetus ('Ok', 'Pending', 'Fail') {
+    print "$stetus\n";
+}
+
+__END__
